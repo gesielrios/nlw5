7d045c8d174401aa4e37fc1d8d61c1755f4bb4bc
Gesiel Rios <gesielrios@gmail.com>
2021-04-21 19:55:38 -0300

    Bug fixes in css.

--- src/pages/epsodes/[slug].tsx
+++ src/pages/epsodes/[slug].tsx
@@ -3,7 +3,6 @@ import ptBR from 'date-fns/locale/pt-BR';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Link from 'next/link';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 import style from './epsode.module.scss';
@@ -18,52 +17,50 @@ type Epsode = {
     durationAsString: string;
     description: string;
     url: string;
-}
+};
 
 type EpsodeProps = {
     epsode: Epsode,
-}
-
-
+};
 
 export default function Epsode({ epsode }:EpsodeProps) {
-    const router = useRouter();
-
     return (
-        <div className={style.epsode}>
-            <div className={style.thumbnailContainer}>
-                <Link href="/">
+        <div className={style.epsodeContainer}>
+            <div className={style.epsode}>
+                <div className={style.thumbnailContainer}>
+                    <Link href="/">
+                        <button type="button">
+                            <img src="/arrow-left.svg" alt="Voltar" />
+                        </button>
+                    </Link>
+                    <Image width={700} height={160} src={epsode.thumbnail} objectFit="cover" />
                     <button type="button">
-                        <img src="/arrow-left.svg" alt="Voltar" />
+                        <img src="/play.svg" alt="Tocar episódio" />
                     </button>
-                </Link>
-                <Image width={700} height={160} src={epsode.thumbnail} objectFit="cover" />
-                <button type="button">
-                    <img src="/play.svg" alt="Tocar episódio" />
-                </button>
+                </div>
+
+                <header>
+                    <h1>{epsode.title}</h1>
+                    <span>{epsode.members}</span>
+                    <span>{epsode.publishedAt}</span>
+                    <span>{epsode.durationAsString}</span>
+                </header>
+
+                <div 
+                    className={style.description} 
+                    dangerouslySetInnerHTML={{ __html: epsode.description}}
+                />
             </div>
-
-            <header>
-                <h1>{epsode.title}</h1>
-                <span>{epsode.members}</span>
-                <span>{epsode.publishedAt}</span>
-                <span>{epsode.durationAsString}</span>
-            </header>
-
-            <div 
-                className={style.description} 
-                dangerouslySetInnerHTML={{ __html: epsode.description}}
-            />
         </div>
-    )
-}
+    );
+};
 
 export const getStaticPaths: GetStaticPaths = async () => {
     return {
         paths: [],
         fallback: 'blocking',
-    }
-}
+    };
+};
 
 export const getStaticProps: GetStaticProps = async (context) => {
     const { slug } = context.params;
@@ -84,5 +81,5 @@ export const getStaticProps: GetStaticProps = async (context) => {
     return {
         props: {epsode},
         revalidate: 60 * 60 * 24,
-    }
-}
+    };
+};
